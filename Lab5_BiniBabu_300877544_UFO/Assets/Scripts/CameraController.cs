af706df0ebbaeac15a0822aa6a91940d77e4d01e
binibabu22 <32741179+binibabu22@users.noreply.github.com>
2018-03-18 20:11:16 -0400

    Latest
    New Lab5

---
+++ Lab5_BiniBabu_300877544_UFO/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+    
+	void Start () {
+        offset = transform.position - player.transform.position; // offset is obtained by subtacting the cameras transform position with player's transform position
+
+	}
+
+    //LateUpdate() used to run after all the child elements in the frame are runnned 
+    void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
